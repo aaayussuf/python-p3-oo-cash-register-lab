326af80ad43bce38ce814b12a315edd1250dfe34
Abdullahi Abdikadir <aabdikadir849@gmail.com>
2025-02-20 14:53:34 +0300

    completed python-p3-oo-cash-register-lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.discount = discount
+        self.items = []
+        self.last_transaction = 0
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.last_transaction = price * quantity
+        self.items.extend([title] * quantity)
+
+    def apply_discount(self):  # Line 15
+        if self.discount:  # Line 16 (properly indented)
+            self.total = int(self.total * (1 - self.discount / 100))
+            print(f"After the discount, the total comes to ${self.total}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        self.total -= self.last_transaction
+        if self.total < 0:
+            self.total = 0
